b24d11f3e8e4093f75d3f0adb468e85b4b010db3
Magic Cat <pusz.monika@gmail.com>
2024-07-23 23:15:47 +0700

    feat: updated node

--- node/node.go
+++ node/node.go
@@ -1,13 +1,25 @@
 package node
 
-import "github.com/ethereum/go-ethereum/ethclient"
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
 type Node struct {
 	client *ethclient.Client
+	ctx    context.Context
 }
 
-func NewNode(client *ethclient.Client) Node {
-	return Node{
-		client: client,
+func NewNode(cfg NodeConfig) (*Node, error) {
+	client, err := ethclient.Dial(cfg.RPC)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the Ethereum client: %s", err)
 	}
+
+	return &Node{
+		ctx:    context.Background(),
+		client: client,
+	}, nil
 }
