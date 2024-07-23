3fd99b3be5288acbd1dad1f86ecf42edadd39fec
Magic Cat <pusz.monika@gmail.com>
2024-07-24 00:45:52 +0700

    feat: add tx saving handler

---
+++ database/transaction.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/MonikaCat/eth-parser/types"
+)
+
+func (db *Database) SaveTransaction(tx types.Transaction) error {
+	txQuery, err := os.ReadFile("database/sql/insert_transaction.sql")
+	if err != nil {
+		return fmt.Errorf("error reading database/sql/insert_transaction.sql: %v", err)
+	}
+
+	_, err = db.SQL.NamedExec(string(txQuery), tx)
+	if err != nil {
+		return fmt.Errorf("error while inserting tx: %v", err)
+	}
+
+	return nil
+}
